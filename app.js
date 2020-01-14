5f3c7029fbfc3e74ff4d9e811495b0a556424a11
Samer Senbol <samer.senbol@gmail.com>
2020-01-14 22:11:57 +0100

    Creating the server.

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from "express";
+import routeRouter from  "./routes/HandleRoutes";
+import tokenRouter from  "./routes/Tokens";
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const PORT = 3000;
+const app = express();
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.use('/token', tokenRouter);
+app.use('/route', routeRouter);
+
+app.all("*",(req, res, next) => {
+  res.status(404).send("page not found");
+});
+
+app.listen(PORT,()=>{
+  console.log("listening http://localhost:"+PORT);
+});
