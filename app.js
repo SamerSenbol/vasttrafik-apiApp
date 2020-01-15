9aeed18ad433cfa7466ee5f78a4033343c43da3a
Samer Senbol <samer.senbol@gmail.com>
2020-01-15 20:23:00 +0100

    Updating the server.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import routeRouter from  "./routes/Stops";
 import tokenRouter from  "./routes/Tokens";
+import tripRouter from "./routes/Trip";
 
 const PORT = 3000;
 const app = express();
@@ -11,6 +12,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/token', tokenRouter);
 app.use('/route', routeRouter);
+app.use('/trip', tripRouter);
 
 app.all("*",(req, res, next) => {
   res.status(404).send("page not found");
