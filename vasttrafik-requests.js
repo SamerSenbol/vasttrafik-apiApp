ed6218534ab9814d5993b7740244d698f67a3481
Samer Senbol <samer.senbol@gmail.com>
2020-01-15 20:03:43 +0100

    Getting trips according to the input from the api.

--- vasttrafik-requests.js
+++ vasttrafik-requests.js
@@ -21,19 +21,41 @@ export default async function updateToken() {
     } catch (err) {
         throw new Error(err);
     }
-    
-    export async function getAllStops(token) {
-        const url = "https://api.vasttrafik.se/bin/rest.exe/v2/location.allstops?format=json";
-        try {
-            //----------- vasttrafik api call to get all stops -----------
-            const res = await axios.get(url, {
-                headers: {
-                    Authorization: "Bearer " + token
-                }
-            });
-            return res.data; //retrun new access_token object
-        } catch (err) {
-            throw new Error(err);
-        }
 
+}
+
+export async function getAllStops(token) {
+    const url = "https://api.vasttrafik.se/bin/rest.exe/v2/location.allstops?format=json";
+    try {
+        //----------- vasttrafik api call to get all stops -----------
+        const res = await axios.get(url, {
+            headers: {
+                Authorization: "Bearer " + token
+            }
+        });
+        return res.data; 
+    } catch (err) {
+        throw new Error(err);
+    }
+}
+
+export async function getTripDetails(token, data) {
+    const url = "https://api.vasttrafik.se/bin/rest.exe/v2/trip?" +
+        "originId=" + data.originId +
+        "&destId=" + data.destId +
+        "&time=" + data.time +
+        "&searchForArrival=" + data.isDepOrArrTime +
+        "&date=" + data.date +
+        "&numTrips=4&needJourneyDetail=0&format=json";
+    try {
+        //----------- vasttrafik api call to get trip details -----------
+        const res = await axios.get(url, {
+            headers: {
+                Authorization: "Bearer " + token
+            }
+        });
+        return res.data;
+    } catch (err) {
+        throw new Error(err);
+    }
 }
