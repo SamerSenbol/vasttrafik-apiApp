623664446da9027d0e4cf5a54d5a1a4510fc2ac6
Samer Senbol <samer.senbol@gmail.com>
2020-01-15 20:15:36 +0100

    Checking if the request contained a token or not.

---
+++ routes/Trip.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import updateToken, { getTripDetails } from '../vasttrafik-requests';
+const router = Router();
+
+/* POST trip details */
+router.post('/', async (req, res, next) => {
+    if (!(req.headers.authorization &&
+        req.headers.authorization.split(" ")[0] === 'Bearer')) {
+        const err = {
+            name: "Missing or malformed Authorization",
+            message: "Header should have Authorization as Bearer Token"
+        };
+        next(err);
+        res.json(err);
+        return;
+    }
