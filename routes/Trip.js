0638520e3f8a6594c2d182035c10919d91382e10
Samer Senbol <samer.senbol@gmail.com>
2020-01-15 20:18:42 +0100

    Getting trip details.

--- routes/Trip.js
+++ routes/Trip.js
@@ -14,5 +14,37 @@ router.post('/', async (req, res, next) => {
         res.json(err);
         return;
     }
+    const token = req.headers.authorization.split(" ")[1]; 
+    const body = req.body;
+    try {
+        console.log("-start-", "\nLoading Trip...");
+        const data = await getTripDetails(token, body);
+        res.json(data); 
+        console.log("Response sent", "-end-");
+    } catch (err) 
+    
+    {
+       
+        if (!err.message.includes("401")) {
+            res.json(err);
+            next(err);
+            return;
+        }
+        console.log("Token expired");
+        try {
+            console.log("Generating new Token...");
+            const new_token_data = await updateToken();
+            const new_token = new_token_data.access_token;
+            console.log("Loading Trip...");
+            let to_send = await getTripDetails(new_token, body); 
+            to_send.token = new_token_data;    
+            res.json(to_send);               
+            console.log("Response sent", "\n-end-");
+        } catch (err) {
+            res.json(err);
+            next(err);
+        }
+    }
+});
 
-    export default router;
+export default router;
