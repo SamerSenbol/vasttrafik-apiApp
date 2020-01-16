1b9592e61710afc279c1d7de1b8241e212add480
Samer Senbol <samer.senbol@gmail.com>
2020-01-16 21:23:01 +0100

    Getting token from the server and storing it in the Localstorage.

--- client/Components/home.jsx
+++ client/Components/home.jsx
@@ -16,6 +16,20 @@ class Home extends Component {
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  render() {
+    return (
+        <div>
+        </div>
+    );
+}
+  getToken = async () => {
+    const res = await fetch("http://localhost:4000/token");
+    const data = await res.json();
+    // console.log(data.access_token);
+    localStorage.setItem("access_token", data.access_token);
+    this.getStops();
+}
 }
 
 export default Home;
